d9bcd3c62b4ce5f0e17e4561be0b0e271cac9ca0
Adrián Carrasco Espinosa <adriancarrascoespinosa@Adrians-Air.home>
2024-04-08 21:59:22 +0200

    image resize removed for bugs

--- frontend/src/components/TextEditor.js
+++ frontend/src/components/TextEditor.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import ReactQuill, { Quill } from 'react-quill';
+import ReactQuill from 'react-quill';
+// Quill
 import 'react-quill/dist/quill.snow.css';
 import { useNavigate } from 'react-router-dom';
 import NoteServiceInstance from '../services/NoteService';
-import ImageResize from 'quill-image-resize-module-react';
+// import ImageResize from 'quill-image-resize-module-react';
 
-window.Quill = Quill
-Quill.register('modules/imageResize', ImageResize);
+// window.Quill = Quill
+// Quill.register('modules/imageResize', ImageResize);
 
 const TextEditor = ({ noteSelected, setReloadNotes, reloadNotes }) => {
 
@@ -44,11 +45,11 @@ const TextEditor = ({ noteSelected, setReloadNotes, reloadNotes }) => {
       ['link', 'image', 'video', 'audio'],
       [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }],
       [{ 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'] }],
-    ],
-    imageResize: {
-      parchment: Quill.import('parchment'),
-      modules: ['Resize', 'DisplaySize']
-    }
+    ]
+    // imageResize: {
+    //   parchment: Quill.import('parchment'),
+    //   modules: ['Resize', 'DisplaySize']
+    // }
   };
 
   const formats = [
@@ -59,6 +60,8 @@ const TextEditor = ({ noteSelected, setReloadNotes, reloadNotes }) => {
   ];
 
   const saveClick = async () => {
+    console.log(content);
+    
     const updatedNote = {
       title: title,
       content: content
