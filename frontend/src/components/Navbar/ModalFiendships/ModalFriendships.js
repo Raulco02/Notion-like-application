2227503f0453f09c3538a666b1247ec95e1665ba
Adrián Carrasco Espinosa <adriancarrascoespinosa@Adrians-Air.home>
2024-05-07 23:21:22 +0200

    backend and frontend integrated

--- frontend/src/components/Navbar/ModalFiendships/ModalFriendships.js
+++ frontend/src/components/Navbar/ModalFiendships/ModalFriendships.js
@@ -1,8 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import FriendShipServiceInstance from '../../../services/FriendShipService';
+
 import './ModalFriendships.css';
 
-const ModalFriendships = ({ handleCloseModalFriends, friends, setFilteredFriends, filteredFriends, friendRequests, setFriendRequest }) => {
+const ModalFriendships = ({ handleCloseModalFriends }) => {
     const [hoveredFriendId, setHoveredFriendId] = useState(null); // Estado para almacenar el ID del amigo sobre el que está el ratón
+    const [searchFriendContent, setSearchFriendContent] = useState(''); // Estado para almacenar el contenido del input de búsqueda
+    const [reloadFriends, setReloadFriends] = useState(false); // Estado para controlar la apertura y cierre de la ventana modal
+    const [friends, setFriends] = useState([]); // Estado para almacenar la lista de amigos
+    const [filteredFriends, setFilteredFriends] = useState([]); // Estado para almacenar la lista de amigos filtrada
+    const [friendRequests, setFriendRequest] = useState([]); // Estado para almacenar la lista de solicitudes de amistad
+    const [mensajeSolicitudAmistadEnviada, setMensajeSolicitudAmistadEnviada] = useState('');
+
+    useEffect(() => {
+        // Código que se ejecutará al montar el componente o cuando alguna de las dependencias cambie
+        getfriends();
+        getfriendRequests();
+
+    }, [reloadFriends]);
+
+
+    const getfriends = async () => {
+
+        const friends = await FriendShipServiceInstance.getUserFriends();
+        console.log("Los amigos son ", friends.data.friends);
+        setFriends(friends.data.friends);
+        setFilteredFriends(friends.data.friends);
+
+    }
+
+    const getfriendRequests = async () => {
+
+        const friendRequests = await FriendShipServiceInstance.getFriendShipRequests();
+        console.log("Las solicitudes son ", friendRequests.data.requests);
+        setFriendRequest(friendRequests.data.requests);
+
+    }
+
+    const handleChangesearchFriendContent = (e) => {
+        const searchTerm = e.target.value;
+        setSearchFriendContent(searchTerm);
+
+        if (searchTerm.trim() === '') {
+            // Si el input está vacío, mostrar todos los amigos sin filtrar
+            setFilteredFriends(friends);
+
+        } else {
+            // Si hay un término de búsqueda, filtrar los amigos según el término
+            const filteredFriends = friends.filter((friend) =>
+                friend.email.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setFilteredFriends(filteredFriends);
+
+        }
+    };
+
+
+    const handleClickRequestFriend = async (content) => {
+        try {
+            await FriendShipServiceInstance.sendFriendShipRequest(content);
+            setMensajeSolicitudAmistadEnviada('s');
+        } catch (error) {
+            console.error('Error sending friend request:', error);
+            setMensajeSolicitudAmistadEnviada('f');
+        }
+    };
+
 
     // Manejador de eventos para cuando el ratón entra al div de amigo
     const handleMouseEnter = (friendId) => {
@@ -14,21 +77,43 @@ const ModalFriendships = ({ handleCloseModalFriends, friends, setFilteredFriends
         setHoveredFriendId(null);
     };
 
-    const declineFriendRequest = (userId) => {
-        //Aquí se eliminaría la solicitud de amistad
-        // Filtrar las solicitudes de amistad para eliminar la solicitud con el ID del usuario
-        const updatedFriendRequests = friendRequests.filter((friendRequest) => friendRequest.id !== userId);
-        // Actualizar el estado de las solicitudes de amistad
-        setFriendRequest(updatedFriendRequests);
+    const declineFriendRequest = async (userId) => {
+
+        try {
+            await FriendShipServiceInstance.acceptFriendShipRequest(userId, "false");
+            setReloadFriends(!reloadFriends);
+        } catch (error) {
+            console.error('Error refusing friend request:', error);
+
+        }
+
     }
 
-    const acceptFriendRequest = (userId) => {
-        //Aquí se aceptaría la solicitud de amistad
-        // Filtrar las solicitudes de amistad para eliminar la solicitud con el ID del usuario
-        const updatedFriendRequests = friendRequests.filter((friendRequest) => friendRequest.id !== userId);
-        // Actualizar el estado de las solicitudes de amistad
-        setFriendRequest(updatedFriendRequests);
+    const acceptFriendRequest = async (userId) => {
 
+        console.log("El id es " + userId);
+
+        try {
+            await FriendShipServiceInstance.acceptFriendShipRequest(userId, "true");
+            setReloadFriends(!reloadFriends);
+
+        } catch (error) {
+            console.error('Error refusing friend request:', error);
+
+        }
+
+    }
+
+    const deleteFriend = async (userId) => {
+
+        try {
+            await FriendShipServiceInstance.deleteFriend(userId);
+            setReloadFriends(!reloadFriends);
+
+        } catch (error) {
+            console.error('Error deleting friend:', error);
+
+        }
 
     }
 
@@ -36,76 +121,95 @@ const ModalFriendships = ({ handleCloseModalFriends, friends, setFilteredFriends
         <div className="modal-friends">
             <span className="close" onClick={handleCloseModalFriends}>&times;</span>
             {/* Buscador por correo electrónico */}
-            <div style={{ display:'flex' }}>
+            <div style={{ display: 'flex' }}>
                 <input
                     style={{ margin: '0.5rem 0 0 0' }}
                     type="text"
-                    placeholder="Search by email..."
-                    onChange={(e) => {
-                        const searchTerm = e.target.value;
-                        // Filtrar la lista de amigos según el término de búsqueda
-                        const filteredFriends = friends.filter((friend) =>
-                            friend.email.toLowerCase().includes(searchTerm.toLowerCase())
-                        );
-                        // Actualizar el estado de amigos filtrados
-                        setFilteredFriends(filteredFriends);
-                    }}
+                    placeholder="Search friend by email..."
+                    onChange={handleChangesearchFriendContent}
                 />
-            
-            {filteredFriends.length === 0  && (
-                <button className='aceptrejectbtn' style={{ display:'flex', alignItems:'center' }}>
-                    <img src="/add-user.png" alt="Add user" className="search" style={{ width: '30px', height: '30px', margin:'0.5rem' }} />
-                </button>
+
+                {((friends.length === 0 && filteredFriends.length === 0) ||
+                    !(friends.length !== 0 && filteredFriends.length !== 0))
+                    && (
+                        <button onClick={() => handleClickRequestFriend(searchFriendContent)} className='aceptrejectbtn' style={{ display: 'flex', alignItems: 'center' }}>
+                            <img src="/add-user.png" alt="Add user" className="search" style={{ width: '30px', height: '30px', margin: '0.5rem' }} />
+                        </button>
+                    )}
+
+            </div>
+
+
+            {mensajeSolicitudAmistadEnviada === 's' && (
+                <div style={{ margin: '1rem 0', marginBottom: '2rem', backgroundColor: 'green', padding: '0.5rem', borderRadius: '5px' }}>
+                    Request submitted successfully
+                </div>
             )}
-            </div>
+
+            {mensajeSolicitudAmistadEnviada === 'f' && (
+                <div style={{ margin: '1rem 0', marginBottom: '2rem', backgroundColor: 'red', padding: '0.5rem', borderRadius: '5px' }}>
+                    Fail sending request
+                </div>
+            )}
 
 
-            <div style={{ margin: '1rem 0', fontSize: '20px' }}>
-                Friend requests:
-            </div>
+            {friendRequests.length > 0 && (
+                <div style={{ margin: '1rem 0', fontSize: '20px' }}>
+                    Friend requests:
+                </div>
+            )}
             {/* Lista de solicitudes de amistad */}
             {/* Iterar sobre el arreglo de solicitudes de amistad y generar un div por cada solicitud */}
-            {friendRequests.map((friendRequest) => (
-                <div key={friendRequest.id} className="friend-container" style={{ display: 'flex', margin: '1rem 0' }}>
+
+            {friendRequests !== undefined && friendRequests.map((friendRequest) => (
+                <div key={friendRequest._id} className="friend-container" style={{ display: 'flex', margin: '1rem 0' }}>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                         <img src="/whiteavatar.png" alt="Avatar" className="avatar" style={{ width: '50px', height: '50px' }} />
                     </div>
                     <div className="friend-info">
-                        <p style={{ margin: '0.5rem' }} className="name">{friendRequest.name}</p>
+                        <p style={{ margin: '0.5rem' }} className="name">{friendRequest.userNamename}</p>
                         <p style={{ margin: '0.5rem' }} className="email">{friendRequest.email}</p>
                     </div>
 
                     <div className='aceptrejectbtn-container'>
                         {/* Aceptar solicitud de amistad */}
-                        <button onClick={() => acceptFriendRequest(friendRequest.id)} className='aceptrejectbtn'>
+                        <button onClick={() => acceptFriendRequest(friendRequest._id)} className='aceptrejectbtn'>
                             <img src="/accept.png" alt="Accept" className="accept" style={{ width: '30px', height: '30px' }} />
                         </button>
                         {/* Rechazar solicitud de amistad */}
-                        <button onClick={() => declineFriendRequest(friendRequest.id)} className='aceptrejectbtn'>
+                        <button onClick={() => declineFriendRequest(friendRequest._id)} className='aceptrejectbtn'>
                             <img src="/decline.png" alt="Reject" className="reject" style={{ width: '30px', height: '30px' }} />
                         </button>
                     </div>
                 </div>
             ))}
 
-            <div style={{ margin: '1rem 0', fontSize: '20px' }}>
-                Friends:
-            </div>
+            {friends.length > 0 ? (
+                <div style={{ margin: '1rem 0', fontSize: '20px' }}>
+                    Friends:
+                </div>
+            ) : (
+                <div style={{ margin: '1rem 0', fontSize: '20px' }}>
+                    You don't have friends yet. Look for some!
+                </div>
+            )}
+
+
             {/* Lista de amigos filtrada */}
             {/* Iterar sobre el arreglo de amigos filtrados y generar un div por cada amigo */}
-            {filteredFriends.map((friend) => (
-                <div key={friend.id} className="friend-container" onMouseEnter={() => handleMouseEnter(friend.id)} onMouseLeave={handleMouseLeave} style={{ display: 'flex', margin: '1rem 0' }}>
+            {filteredFriends !== undefined && filteredFriends.map((friend) => (
+                <div key={friend._id} className="friend-container" onMouseEnter={() => handleMouseEnter(friend._id)} onMouseLeave={handleMouseLeave} style={{ display: 'flex', margin: '1rem 0' }}>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                         <img src="/whiteavatar.png" alt="Avatar" className="avatar" style={{ width: '50px', height: '50px' }} />
                     </div>
                     <div className="friend-info">
-                        <p style={{ margin: '0.5rem' }} className="name">{friend.name}</p>
+                        <p style={{ margin: '0.5rem' }} className="name">{friend.userName}</p>
                         <p style={{ margin: '0.5rem' }} className="email">{friend.email}</p>
                     </div>
 
-                    {hoveredFriendId === friend.id && (
+                    {hoveredFriendId === friend._id && (
                         <div className='delete-friend-container'>
-                            <button className='aceptrejectbtn'>
+                            <button onClick={() => deleteFriend(friend._id)} className='aceptrejectbtn'>
                                 <img src="/bin.png" alt="Remove" className="delete-friend-image" style={{ width: '30px', height: '30px' }} />
                             </button>
                         </div>
--- frontend/src/services/FriendShipService.js
+++ frontend/src/services/FriendShipService.js
@@ -5,7 +5,7 @@ const USER_BASE_REST_API_URL = "http://localhost:3001/users";
 class FriendShipService {
 
     sendFriendShipRequest(email) {
-        return axios.post(USER_BASE_REST_API_URL + '/friendship_request', { email: email }, { withCredentials: true });
+        return axios.post(USER_BASE_REST_API_URL + '/friendship_request', { requestedUserEmail: email }, { withCredentials: true });
     }
     
     getFriendShipRequests() {
@@ -13,16 +13,20 @@ class FriendShipService {
     }
 
     acceptFriendShipRequest(userId, accepted) {
-        return axios.post(USER_BASE_REST_API_URL + '/accept_friendship_request', { userId: userId, accepted: accepted }, { withCredentials: true });
+        return axios.post(USER_BASE_REST_API_URL + '/set_friendship_request', { userId: userId, accepted: accepted }, { withCredentials: true });
     }
 
     getUserFriends() {
         return axios.get(USER_BASE_REST_API_URL + '/get_friends', { withCredentials: true });
     }
 
-    getUserDeleteFriend(userId) {
-        return axios.delete(USER_BASE_REST_API_URL + '/delete_friend',{ userId: userId }, { withCredentials: true });
+    deleteFriend(userId) {
+        return axios.delete(USER_BASE_REST_API_URL + '/delete_friend', { 
+            data: { userId: userId }, 
+            withCredentials: true 
+        });
     }
+    
 
 }
 
